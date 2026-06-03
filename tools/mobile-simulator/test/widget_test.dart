6fa8a3b6b07057ef14ec08784e2983751667ba26
rannymphaea <unandnewgame@gmail.com>
2026-06-03 15:05:43 +0700

    miaw

--- tools/mobile-simulator/test/widget_test.dart
+++ tools/mobile-simulator/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:newgame_mobile_simulator/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(NewgameSimApp());
+    await tester.pumpWidget(const NewgameSimApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
